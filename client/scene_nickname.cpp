9d0e86b57342ba651a61a380e0713e7d032528cb
pixelsapphire <rubynaxela@gmail.com>
2024-01-12 01:11:12 +0100

    Constraint nickname to printable characters only

--- client/scene_nickname.cpp
+++ client/scene_nickname.cpp
@@ -49,9 +49,9 @@ snek::scene_nickname::scene_nickname() : nickname_prompt(snek::make_entity<sf::T
 bool snek::scene_nickname::handle_event(const sf::Event& event) {
     if (event.type == sf::Event::TextEntered) {
         if (event.text.unicode == 8 and not nickname.empty()) nickname.pop_back();
-        else if (event.text.unicode < 128) nickname += static_cast<char>(event.text.unicode);
+        else if (event.text.unicode >= 32 and event.text.unicode <= 126) nickname += char(event.text.unicode);
         (*nickname_view)->setString(nickname);
-        (*nickname_view)->setOrigin(nickname_view->center());
+        (*nickname_view)->setOrigin(nickname_view->center().x, 24);
     } else if (event.type == sf::Event::MouseButtonPressed) {
         if (event.mouseButton.button == sf::Mouse::Left and
             (*accept_button)->getGlobalBounds().contains(snek::conv::to_vec2f(event.mouseButton)))
