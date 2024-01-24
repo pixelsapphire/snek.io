c2c945c566ca69e64fcb8bf98ad4e9fe87426b47
pixelsapphire <rubynaxela@gmail.com>
2024-01-24 22:20:08 +0100

    Fix the random_value function

--- server/utility.hpp
+++ server/utility.hpp
@@ -13,11 +13,10 @@ namespace snek {
     T random_value(T min, T max) {
         static std::random_device rd{};
         static std::mt19937 rng(rd());
-        static std::uniform_int_distribution<T> dist{min, max};
-        return dist(rng);
+        return std::uniform_int_distribution<T>{min, max}(rng);
     }
 
-    bool is_nearby (const snek::vector_2f& position, const snek::vector_2f& segment, float distance);
+    bool is_nearby(const snek::vector_2f& position, const snek::vector_2f& segment, float distance);
 }
 
 #endif //SNEK_IO_UTILITY_HPP
