4cb6569174c135ae46393e5a183b03b0ad2a688f
pixelsapphire <rubynaxela@gmail.com>
2024-01-26 01:24:56 +0100

    Add the __clang_std macro

--- client/utility.cpp
+++ client/utility.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <sstream>
 #include <string>
+#include "common_utility.hpp"
 #include "utility.hpp"
 
 sf::Vector2f snek::conv::to_vec2f(const sf::Event::MouseButtonEvent& ev) { return {float(ev.x), float(ev.y)}; }
@@ -10,12 +11,12 @@ bool snek::event::key_pressed(const sf::Event& ev, sf::Keyboard::Key key) {
 }
 
 sf::Vector2f snek::math::direction_change(const sf::Vector2f& origin, const sf::Vector2f& target, float delta_max) {
-    float delta = atan2f(target.y, target.x) - atan2f(origin.y, origin.x);
+    float delta = __clang_std::atan2f(target.y, target.x) - __clang_std::atan2f(origin.y, origin.x);
     if (delta > M_PI) delta -= 2 * M_PI;
     else if (delta < -M_PI) delta += 2 * M_PI;
     delta = std::clamp(delta, -delta_max, delta_max);
-    return {origin.x * cosf(delta) - origin.y * sinf(delta),
-            origin.x * sinf(delta) + origin.y * cosf(delta)};
+    return {origin.x * __clang_std::cosf(delta) - origin.y * __clang_std::sinf(delta),
+            origin.x * __clang_std::sinf(delta) + origin.y * __clang_std::cosf(delta)};
 }
 
 sf::Vector2f snek::serial::decode_vector(const std::string& vector_data) {
---
+++ common/common_utility.hpp
@@ -0,0 +1,10 @@
+#ifndef SNEK_IO_COMMON_UTILITY_HPP
+#define SNEK_IO_COMMON_UTILITY_HPP
+
+#ifdef __clang__
+#define __clang_std std
+#else
+#define __clang_std
+#endif
+
+#endif // SNEK_IO_COMMON_UTILITY_HPP
